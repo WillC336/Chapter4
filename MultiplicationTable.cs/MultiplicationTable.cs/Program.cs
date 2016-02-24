5d593e3b847839b4e413aa17f13a54e2b8f0dbd8
WillC336 <wclayton1166@gmail.com>
2016-02-24 11:31:19 -0500

    Initial Commit

---
+++ MultiplicationTable.cs/MultiplicationTable.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 1; y <= 10; y++)
+            {
+                for (int x = 1; x <= 10; x++)
+                {
+                    Console.Write(y * x + "\t");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
